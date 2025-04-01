0d2fd7d369569b54e732112f6a3a44fce34c614a
phevosccp <160875214+phevosccp@users.noreply.github.com>
2025-04-01 11:30:41 +0000

    include shared interfaces

---
+++ include/ITr2Audio.h
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////
+//
+//    Creator:   CCP Team Engine
+//    Created:   April 2020
+//    Modified:  March 2025
+//    Copyright: CCP 2020-2025
+//
+//    Description:
+//      An interface allowing for an abstraction of audio on an asset.
+
+#pragma once
+
+#include "ITr2AudEmitter.h"
+
+BLUE_INTERFACE( ITr2Audio ) : public IRoot
+{
+	virtual void Update( Vector3& sourcePosition, Vector3& destPosition) = 0;
+	virtual ITr2AudEmitterPtr FindEmitterByName( const char* name ) = 0;
+};
